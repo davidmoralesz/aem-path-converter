dabd182308b00466a2de459b6d07f82afb8cd4ce
david morales <118213696+davidmoralesz@users.noreply.github.com>
2025-03-06 09:29:47 -0600

    chore: Add config to gitignore

--- path_converter/config.py
+++
@@ -1,8 +0,0 @@
-# Default domain used for relative paths
-DEFAULT_DOMAIN = "domain.com"
-
-# Hotkey assignment for triggering the conversion
-HOTKEY = "<cmd>+<shift>+."
-
-# Base path used for building the AEM path
-BASE_PATH = "/content/"
